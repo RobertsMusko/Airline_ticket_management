c1deb11584b020adf581ea6271aace3c69f33227
rmusk <rmusk@LAPTOP-VOB00DV6>
2023-12-04 20:27:54 +0100

    Add project files.

---
+++ Common/Models/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Row {  get; set; }    
+        public string Column { get; set; }
+
+        [ForeignKey("Flight")]
+        public int FlightIdFK { get; set; }
+        public Flight Flight { get; set; }
+
+
+        public int Passaport {  get; set; }
+        public double PricePaid {  get; set; }
+        public Boolean Cancelled {  get; set; } 
+
+    }
+}
---
+++ Data/Context/AirlineDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class AirlineDbContext : DbContext
+    {
+        public AirlineDbContext(DbContextOptions<AirlineDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
+
